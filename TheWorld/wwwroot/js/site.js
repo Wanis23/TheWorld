89a99e15939c382f8c7a0cdfc4ab2dacac4584a8
wanis chebki <wanischebki@hotmail.com>
2017-08-28 16:25:19 +0200

    1st commit

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// site.js 
+
+(function () {
+
+    var ele = $("#username");
+    ele.text("Vinyl World");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.css("background-color", "#ccc");
+    });
+
+    main.on("mouseleave", function () {
+        main.css("background-color", "");
+    });
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("show sidebar");
+        }
+        else {
+            $(this).text("hide-sidebar");
+        }
+    });
+
+})();
